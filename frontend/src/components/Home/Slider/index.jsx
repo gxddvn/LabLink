868b5538281fd90c37ee1d7c0aa2701274f5c798
Bogdan Maltsev <75526809+gxddvn@users.noreply.github.com>
2024-05-22 16:12:11 +0300

    Update Slider.
    - Fix Slider func for button "<"

--- frontend/src/components/Home/Slider/index.jsx
+++ frontend/src/components/Home/Slider/index.jsx
@@ -6,7 +6,7 @@ const Slider = ({img_list = [], }) => {
         <div className='flex relative'>
             <img className='max-w-2xl' src={`src/components/Home/${img_list[isSlide]}`} alt="" />
             <div className='absolute w-full h-full flex justify-between'>
-                <button className='px-3 bg-[rgba(17,24,39,0.3)] transition-all ease-linear hover:bg-[rgba(17,24,39,0.5)]' onClick={() => isSlide>0 ? setIsSlide(isSlide-1) : setIsSlide(0)}>{`<`}</button>
+                <button className='px-3 bg-[rgba(17,24,39,0.3)] transition-all ease-linear hover:bg-[rgba(17,24,39,0.5)]' onClick={() => isSlide>0 ? setIsSlide(isSlide-1) : setIsSlide(img_list.length-1)}>{`<`}</button>
                 <div className='flex items-end'>
                     {img_list && img_list?.map((img,index) => (
                         <div className={isSlide===index ? `text-gray-900` : `text-white`} key={index}>-</div>
